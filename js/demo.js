01552cdd37da0199993768dd7b6f0ab113fa7ff5
Anand RM <anand-r-m@users.noreply.github.com>
2026-05-13 12:18:41 +0530

    demo video logic updated

--- js/demo.js
+++ js/demo.js
@@ -1,12 +1,12 @@
-const video = document.getElementById('demo-video');
-const placeholder = document.getElementById('demo-placeholder');
+var video = document.getElementById('demo-video');
+var placeholder = document.getElementById('demo-placeholder');
 
-video.addEventListener('canplay', () => {
+video.addEventListener('canplay', function() {
   video.classList.remove('d-none');
   placeholder.classList.add('d-none');
 });
 
-video.addEventListener('error', () => {
+video.addEventListener('error', function() {
   video.classList.add('d-none');
   placeholder.classList.remove('d-none');
 });
